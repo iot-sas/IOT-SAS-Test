6fa55fe7ea64d069ff3cef1c40532839268895e7
Stuart Johnson <stuart@factoid.org>
2019-04-06 00:54:15 +0100

    first commit

---
+++ IOT-SAS.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using FactomSharp.Factomd;
+
+namespace IOTSASTest
+{
+    public class IOT_SAS : IDisposable
+    {
+        SerialPort mySerial;
+        
+    
+        public IOT_SAS(String device, uint baudRate = 57600)
+        {
+
+            mySerial = new SerialPort(device, (int)baudRate, Parity.None, 8, StopBits.One);
+            
+            mySerial.Handshake = Handshake.None;
+            mySerial.Open();
+        
+        }
+
+        private string GetPublicECAddress()
+        {
+            mySerial.DiscardInBuffer();
+            var GetEC = new byte[] { 0xFA, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00 };
+            mySerial.Write(GetEC, 0, GetEC.Length);
+
+            var key = new byte[52];
+            
+            var count = 0;
+            var bytesRx = 0;
+            while (count < key.Length)
+            {
+                bytesRx = mySerial.Read(key, count, key.Length-count);
+                count += bytesRx;
+            }
+
+            return Encoding.ASCII.GetString(key);
+        }
+
+        public FactomSharp.ECAddress GetECAddress(FactomdRestClient factomd)
+        {
+            var ecAddress = new FactomSharp.ECAddress(factomd, GetPublicECAddress());
+            ecAddress.SignFunction = (data) =>
+            {
+                    return SignChain(data);
+            };
+            return ecAddress;
+        }
+        
+
+        public byte[] SignChain(byte[] data)
+        {
+            mySerial.DiscardInBuffer();
+            var toSign = new byte[data.Length + 5];
+            toSign[0] = 0xFA;
+            toSign[1] = 0x02;
+            toSign[2] = 0x02;
+            toSign[3] = 0x0;
+            toSign[4] = (byte)data.Length;
+            Array.Copy(data, 0, toSign, 5, data.Length);            
+            
+            mySerial.Write(toSign, 0, toSign.Length);
+
+            var signature = new byte[64];
+            
+            var count = 0;
+            var bytesRx = 0;
+            while (count < signature.Length)
+            {
+                bytesRx = mySerial.Read(signature, count, signature.Length-count);
+                count += bytesRx;
+            }
+            
+            return signature;
+        }
+        
+        public byte[] SignEntry(byte[] data)
+        {
+            mySerial.DiscardInBuffer();
+            var toSign = new byte[data.Length + 5];
+            toSign[0] = 0xFA;
+            toSign[1] = 0x02;
+            toSign[2] = 0x01;
+            toSign[3] = 0x0;
+            toSign[4] = (byte)data.Length;
+            Array.Copy(data, 0, toSign, 5, data.Length);            
+            
+            mySerial.Write(toSign, 0, toSign.Length);
+
+            Thread.Sleep(1000);
+
+            var signature = new byte[64];
+
+            var count = 0;
+            var bytesRx = 0;
+            while (count < signature.Length)
+            {
+                bytesRx = mySerial.Read(signature, count, signature.Length-count);
+                count += bytesRx;
+            }
+            
+            return signature;
+        }
+
+        public void Dispose()
+        {
+            mySerial.Close();
+        }
+    }    
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FactomSharp;
+using FactomSharp.Factomd;
+
+namespace IOTSASTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Open a connection to the IOT-SAS board
+            using (var iotsas = new IOT_SAS("/dev/ttyAMA0", 57600))
+            {
+                //Open a connection to a Factom node
+                var factomd = new FactomdRestClient("https://api.factomd.net/v2");
+
+                //Get an EC address from the IOS-SAS
+                var address = iotsas.GetECAddress(factomd);  
+                    
+                Console.WriteLine($"EC Address: {address.Public}");
+                Console.WriteLine($"Balance {address.GetBalance()}");
+
+                using (var chain = new Chain(address))
+                {
+                    chain.ChainStatusChange += (o, a) =>
+                    {
+                        Console.WriteLine($"Chain: {a.ToString()}");
+                    };
+
+                    chain.QueueItemStatusChange += (o, a) =>
+                    {
+                        var item = (EntryItem)o;
+                        Console.WriteLine($"Entry: {a.ToString()} {item?.ApiError?.error}");
+                    };
+
+                    chain.Create(Encoding.ASCII.GetBytes("This is my new chain"));
+
+                    var text = $"This is a test - hello!";
+
+                    var entry = chain.AddEntry(Encoding.ASCII.GetBytes(text));
+                    Console.WriteLine($"ChainID: {chain.ChainID}");
+
+                    Console.ReadLine(); //Pause
+                }
+            }
+        }
+    }
+}
